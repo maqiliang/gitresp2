412acbbec94928fd6c0123782e423ea8f9f221ee
mql <mqlkov@163.com>
2021-12-07 19:29:11 +0800

    修改了Student类，添加了年龄，姓名，分数等属性

--- test01/src/com/mql/Student.java
+++ test01/src/com/mql/Student.java
@@ -10,4 +10,10 @@ public class Student {
     private int age;
     private String name;
     private double score;
+
+    public Student(int age, String name, double score) {
+        this.age = age;
+        this.name = name;
+        this.score = score;
+    }
 }
